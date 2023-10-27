887878eadd8913656b1c4e9a721c48ffc1bbffcd
filipebratti <filipebratti@hotmail.com>
2023-10-27 15:12:28 -0300

    fix database config

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -7,6 +7,7 @@ module.exports = {
   username: process.env.USERNAMEDB, 
   password: process.env.PASSWORDDB, 
   database: process.env.DATABASE,
+  database_url: process.env.DATABASE_URL,
   port: process.env.PORT, 
   secret_key : process.env.JWT_SECRET,
   define: {
