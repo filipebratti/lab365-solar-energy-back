83bff466da711f435b9926f9c5820c02f7348549
filipebratti <filipebratti@hotmail.com>
2023-10-27 15:48:55 -0300

    add pg-connection-string-parse

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -1,16 +1,18 @@
-const { config } = require('dotenv')
-config()
+const { config } = require("dotenv");
+config();
+var parse = require('pg-connection-string').parse;
+var configdatabase = parse(process.env.HOST);
 
 module.exports = {
-  dialect: process.env.DIALECT, 
-  host: process.env.HOST, 
-  username: process.env.USERNAMEDB, 
-  password: process.env.PASSWORDDB, 
-  database: process.env.DATABASE,  
-  port: process.env.PORT, 
-  secret_key : process.env.JWT_SECRET,
+  dialect: "postgres", //Qual banco de dados está utilizando;
+  host: configdatabase.host, //Qual servidor está utilizando;
+  username: configdatabase.user, //Qual o nome do seu usuário no postgres;
+  password: configdatabase.password, //Qual a senha do seu usuário no postgres;
+  database: configdatabase.database, //Qual o nome do seu database no postgres;
+  port: configdatabase.port, //Qual porta do seu postgres (Normalmente é a 5432);
   define: {
     underscored: true,
-    underscoredAll: true
-  }
+    underscoredAll: true,
+    freezeTableName: true,
+  },
 };
