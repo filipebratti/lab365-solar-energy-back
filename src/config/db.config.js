2b7b1a721173710d17fcc0d7b27c96f28462ea39
filipebratti <filipebratti@hotmail.com>
2023-10-27 15:56:14 -0300

    add ssl config

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -4,15 +4,20 @@ var parse = require('pg-connection-string').parse;
 var configdatabase = parse(process.env.HOST);
 
 module.exports = {
-  dialect: "postgres", //Qual banco de dados está utilizando;
-  host: configdatabase.host, //Qual servidor está utilizando;
-  username: configdatabase.user, //Qual o nome do seu usuário no postgres;
-  password: configdatabase.password, //Qual a senha do seu usuário no postgres;
-  database: configdatabase.database, //Qual o nome do seu database no postgres;
-  port: configdatabase.port, //Qual porta do seu postgres (Normalmente é a 5432);
+  dialect: 'postgres',
+  host: process.env.HOST,
+  username: process.env.USERNAMEDB,
+  password: process.env.PASSWORDDB,
+  database: process.env.DATABASE,
+  port: process.env.PORT,
+  ssl: true, // Habilita o uso de SSL
+  dialectOptions: {
+    ssl: {
+      require: true, // Define o sslmode para "require"
+    },
+  },
   define: {
     underscored: true,
     underscoredAll: true,
-    freezeTableName: true,
-  },
+  }
 };
